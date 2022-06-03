cd16eb387a5178afef9abe37e88f9838f0af5018
Mathieu Moreault <seranx@gmail.com>
2022-06-03 11:42:57 -0400

    Added extensions to quickly surround strings with DML tags.

---
+++ DML.NET.Tests/StringExtensionsTester.cs
@@ -0,0 +1,218 @@
+﻿namespace DML.NET.Tests;
+
+[TestClass]
+public class StringExtensionsTester
+{
+    [TestClass]
+    public class Color_Rgb : Tester
+    {
+        [TestMethod]
+        public void Always_SurroundStringWithColor()
+        {
+            //Arrange
+            var value = Fixture.Create<string>();
+            var red = Fixture.Create<byte>();
+            var green = Fixture.Create<byte>();
+            var blue = Fixture.Create<byte>();
+
+            //Act
+            var result = value.Color(red, green, blue);
+
+            //Assert
+            result.Should().Be($"<color red={red} green={green} blue={blue} alpha=255>{value}</color>");
+        }
+    }
+
+    [TestClass]
+    public class Color_Struct : Tester
+    {
+        [TestMethod]
+        public void Always_SurroundStringWithColor()
+        {
+            //Arrange
+            var value = Fixture.Create<string>();
+            var color = Fixture.Create<Color>();
+
+            //Act
+            var result = value.Color(color);
+
+            //Assert
+            result.Should().Be($"<color red={color.Red} green={color.Green} blue={color.Blue} alpha={color.Alpha}>{value}</color>");
+        }
+    }
+
+    [TestClass]
+    public class Highlight_Rbg : Tester
+    {
+        [TestMethod]
+        public void Always_SurroundStringWithColor()
+        {
+            //Arrange
+            var value = Fixture.Create<string>();
+            var red = Fixture.Create<byte>();
+            var green = Fixture.Create<byte>();
+            var blue = Fixture.Create<byte>();
+
+            //Act
+            var result = value.Highlight(red, green, blue);
+
+            //Assert
+            result.Should().Be($"<highlight red={red} green={green} blue={blue} alpha=255>{value}</highlight>");
+        }
+    }
+
+    [TestClass]
+    public class Highlight_Struct : Tester
+    {
+        [TestMethod]
+        public void Always_SurroundStringWithColor()
+        {
+            //Arrange
+            var value = Fixture.Create<string>();
+            var color = Fixture.Create<Color>();
+
+            //Act
+            var result = value.Highlight(color);
+
+            //Assert
+            result.Should().Be($"<highlight red={color.Red} green={color.Green} blue={color.Blue} alpha={color.Alpha}>{value}</highlight>");
+        }
+    }
+
+    [TestClass]
+    public class Style : Tester
+    {
+        [TestMethod]
+        public void WhenStyleIsBold_SurroundWithBold()
+        {
+            //Arrange
+            var value = Fixture.Create<string>();
+
+            //Act
+            var result = value.Style(TextStyle.Bold);
+
+            //Assert
+            result.Should().Be($"<bold>{value}</bold>");
+        }
+
+        [TestMethod]
+        public void WhenStyleIsItalic_SurroundWithItalic()
+        {
+            //Arrange
+            var value = Fixture.Create<string>();
+
+            //Act
+            var result = value.Style(TextStyle.Italic);
+
+            //Assert
+            result.Should().Be($"<italic>{value}</italic>");
+        }
+
+        [TestMethod]
+        public void WhenStyleIsStrikeout_SurroundWithStrikeout()
+        {
+            //Arrange
+            var value = Fixture.Create<string>();
+
+            //Act
+            var result = value.Style(TextStyle.Strikeout);
+
+            //Assert
+            result.Should().Be($"<strikeout>{value}</strikeout>");
+        }
+
+        [TestMethod]
+        public void WhenStyleIsUnderline_SurroundWithUnderline()
+        {
+            //Arrange
+            var value = Fixture.Create<string>();
+
+            //Act
+            var result = value.Style(TextStyle.Underline);
+
+            //Assert
+            result.Should().Be($"<underline>{value}</underline>");
+        }
+
+        [TestMethod]
+        public void WhenOutOfRange_Throw()
+        {
+            //Arrange
+            var value = Fixture.Create<string>();
+
+            //Act
+            var action = () => value.Style((TextStyle)int.MaxValue);
+
+            //Assert
+            action.Should().Throw<ArgumentOutOfRangeException>().WithParameterName("style");
+        }
+    }
+
+    [TestClass]
+    public class Bold : Tester
+    {
+        [TestMethod]
+        public void Always_SurroundWithBold()
+        {
+            //Arrange
+            var value = Fixture.Create<string>();
+
+            //Act
+            var result = value.Bold();
+
+            //Assert
+            result.Should().Be($"<bold>{value}</bold>");
+        }
+    }
+
+    [TestClass]
+    public class Italic : Tester
+    {
+        [TestMethod]
+        public void Always_SurroundWithItalic()
+        {
+            //Arrange
+            var value = Fixture.Create<string>();
+
+            //Act
+            var result = value.Italic();
+
+            //Assert
+            result.Should().Be($"<italic>{value}</italic>");
+        }
+    }
+
+    [TestClass]
+    public class Strikeout : Tester
+    {
+        [TestMethod]
+        public void Always_SurroundWithStrikeout()
+        {
+            //Arrange
+            var value = Fixture.Create<string>();
+
+            //Act
+            var result = value.Strikeout();
+
+            //Assert
+            result.Should().Be($"<strikeout>{value}</strikeout>");
+        }
+    }
+
+    [TestClass]
+    public class Underline : Tester
+    {
+        [TestMethod]
+        public void Always_SurroundWithUnderline()
+        {
+            //Arrange
+            var value = Fixture.Create<string>();
+
+            //Act
+            var result = value.Underline();
+
+            //Assert
+            result.Should().Be($"<underline>{value}</underline>");
+        }
+    }
+}
---
+++ DML.NET/StringExtensions.cs
@@ -0,0 +1,70 @@
+﻿namespace ToolBX.DML.NET;
+
+public static class StringExtensions
+{
+    /// <summary>
+    /// Surrounds the string with the 'color' DML tag 
+    /// </summary>
+    public static string Color(this string value, byte red, byte green, byte blue, byte alpha = byte.MaxValue) => value.Color(new Color(red, green, blue, alpha));
+
+    /// <summary>
+    /// Surrounds the string with the 'color' DML tag 
+    /// </summary>
+    public static string Color(this string value, Color color)
+    {
+        return $"<{DmlTags.Color} red={color.Red} green={color.Green} blue={color.Blue} alpha={color.Alpha}>{value}</{DmlTags.Color}>";
+    }
+
+    /// <summary>
+    /// Surrounds the string with the 'highlight' DML tag 
+    /// </summary>
+    public static string Highlight(this string value, byte red, byte green, byte blue, byte alpha = byte.MaxValue) => value.Highlight(new Color(red, green, blue, alpha));
+
+    /// <summary>
+    /// Surrounds the string with the 'highlight' DML tag 
+    /// </summary>
+    public static string Highlight(this string value, Color color)
+    {
+        return $"<{DmlTags.Highlight} red={color.Red} green={color.Green} blue={color.Blue} alpha={color.Alpha}>{value}</{DmlTags.Highlight}>";
+    }
+
+    /// <summary>
+    /// Surrounds the string with a DML text style tag
+    /// </summary>
+    public static string Style(this string value, TextStyle style)
+    {
+        switch (style)
+        {
+            case TextStyle.Bold:
+                return value.Bold();
+            case TextStyle.Italic:
+                return value.Italic();
+            case TextStyle.Underline:
+                return value.Underline();
+            case TextStyle.Strikeout:
+                return value.Strikeout();
+            default:
+                throw new ArgumentOutOfRangeException(nameof(style), style, null);
+        }
+    }
+
+    /// <summary>
+    /// Surrounds the string with the 'bold' DML tag 
+    /// </summary>
+    public static string Bold(this string value) => $"<{DmlTags.Bold}>{value}</{DmlTags.Bold}>";
+
+    /// <summary>
+    /// Surrounds the string with the 'italic' DML tag 
+    /// </summary>
+    public static string Italic(this string value) => $"<{DmlTags.Italic}>{value}</{DmlTags.Italic}>";
+
+    /// <summary>
+    /// Surrounds the string with the 'strikeout' DML tag 
+    /// </summary>
+    public static string Strikeout(this string value) => $"<{DmlTags.Strikeout}>{value}</{DmlTags.Strikeout}>";
+
+    /// <summary>
+    /// Surrounds the string with the 'underline' DML tag 
+    /// </summary>
+    public static string Underline(this string value) => $"<{DmlTags.Underline}>{value}</{DmlTags.Underline}>";
+}
